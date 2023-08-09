180063e3928e1ab7fd2d9e4cb8c6ce85c0a8f0be
Andrew Persaud <andreew.j.persaud@gmail.com>
2023-08-08 21:18:56 -0400

    Start the settings screen and interface

---
+++ src/screens/Settings.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react"
+
+interface Settings {
+    columns: number;
+    colourScheme: string;
+    impossibleMode: boolean;
+    startImpossibleMode: boolean;
+    open: boolean;
+    close: boolean;
+    boardWidth: number;
+    width: number;
+    height: number;
+    updateSettings: void;
+}
+
+export const Settings = () => {
+
+}
